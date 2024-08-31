361f62da515d9da6f3a46d812c775adef5781955
Alejandro Benítez Bravo <a01712835@tec.mx>
2024-08-30 22:11:36 -0600

    Added if statements

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,31 +1,32 @@
-# ========== FUNCIONES ===========
+# ========== FUNCIONES =========== #
+
 
 # Función para dibujar el tablero vacío
 def print_board():
-    print("       |       |       ")
-    print("       |       |       ")
-    print("       |       |       ")
-    print("-------|-------|-------")
-    print("       |       |       ")
-    print("       |       |       ")
-    print("       |       |       ")
-    print("-------|-------|-------")
-    print("       |       |       ")
-    print("       |       |       ")
-    print("       |       |       ")
+    print("         |         |         ")
+    print("         |         |         ")
+    print("         |         |         ")
+    print("---------|---------|---------")
+    print("         |         |         ")
+    print("         |         |         ")
+    print("         |         |         ")
+    print("---------|---------|---------")
+    print("         |         |         ")
+    print("         |         |         ")
+    print("         |         |         ")
 
-# Función para imprimir número de juego
+# Función para imprimir número de juego 
 def print_game(number):
     print('\n---------- GAME', number, '---------- \n \n')
 
 # Imprimir quién ganó y aumentar su puntaje
 # Esta función va tomar como parámetro winner, que va a ser un valor booleano
-def winner(winner):
+def winner(player_1_won):
     global player_1
     global player_2
     global player_1_score
     global player_2_score
-    if winner:
+    if player_1_won:
         print(player_1, 'wins this game!')
         player_1_score += 1
 
@@ -33,12 +34,36 @@ def winner(winner):
         print(player_2, 'wins this game!')
         player_2_score += 1
 
-# ============= PUNTAJE ==============
+
+# ============= RONDAS =============== #
+
+
+# Declarar la primera ronda como 1
+round = 1
+
+
+def is_player_1_turn():
+    global round
+    # Si el número de ronda no es par es turno del jugador 1
+    if round % 2 != 0:
+        round += 1
+        return True
+    # Si el número de ronda es par es turno del jugador 2
+    else:
+        round += 1
+        return False
+
+
+
+# ============= PUNTAJE ============== #
+
 
 player_1_score = 0
 player_2_score = 0
 
-# ============== JUEGO ===============
+
+# ============== JUEGO =============== #
+
 
 # Elección de nombre
 player_1 = input('Choose player 1 name: ')
@@ -48,6 +73,8 @@ player_2 = input('Choose player 2 name: ')
 
 # Imprimir el número del juego
 print_game(1)
+
+print_board()
 
 # Suponemos que el jugador 1 ganó
 winner(True)
